cfeb8186a6512ad45ad58c6cb4eca8ad5176c711
Aymen <154266704+AymenBoubetana@users.noreply.github.com>
2024-08-02 12:50:20 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["https://aymeni-systeme-tickets.netlify.app","http://localhost:3000", "http://localhost:3001"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
